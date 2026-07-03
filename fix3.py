78a42ad9111493f576773be9c3191a172df7f077
UserFX <user18fx@gmail.com>
2026-07-03 15:01:58 -0400

    chore: remove temp scripts

--- fix3.py
+++
@@ -1,6 +0,0 @@
-﻿lines=open('index.html',encoding='utf-8').read().split('\n')
-lines[887]=lines[887].strip()
-if lines[887] in ['u{448}','?',''] or len(lines[887])<3:
-    lines[887]=''
-open('index.html','w',encoding='utf-8').write('\n'.join(lines))
-print('line 888:',repr(lines[887]))
