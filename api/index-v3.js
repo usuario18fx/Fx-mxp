c0c09c3776568cd9633e9e2b784296d441f99740
User Fx <234803878+usuario18fx@users.noreply.github.com>
2026-09-07 05:16:58 -0400

    Fix CARTO tile URLs for FX Map

--- api/index-v3.js
+++ api/index-v3.js
@@ -1,7 +1,7 @@
 const v2 = require('./index-v2');
 
 function patchMapHtml(html) {
-  const rasterStyle = `{version:8,sources:{fxRaster:{type:'raster',tiles:['https://a.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png','https://b.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png','https://c.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png'],tileSize:256,attribution:'© OpenStreetMap © CARTO'}},layers:[{id:'fxRaster',type:'raster',source:'fxRaster',minzoom:0,maxzoom:22}]}`;
+  const rasterStyle = `{version:8,sources:{fxRaster:{type:'raster',tiles:['https://a.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png','https://b.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png','https://c.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png'],tileSize:256,attribution:'© OpenStreetMap © CARTO'}},layers:[{id:'fxRaster',type:'raster',source:'fxRaster',minzoom:0,maxzoom:22}]}`;
 
   html = html.replace(
     '#map{position:absolute;inset:0}',
